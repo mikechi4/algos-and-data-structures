c9a6c1081173e7cd3daf8989ad5ef433684b3545
Mike Chi <mikechi@macbook-pro-4.lan>
2020-05-16 12:10:50 -0700

    adding selectionSort

---
+++ sorting/selectionSort.js
@@ -0,0 +1,34 @@
+// similar to bubble sort. instead of moving larger valuse into sorted position,  we  place 
+// smaller ones into sorted positions
+
+// still moving from beginning to end, slightly diff approach...
+
+// generic swap function 
+const swap = (arr, indexA, indexB) => {
+    let temp = arr[indexA];
+    arr[indexA] = arr[indexB];
+    arr[indexB] = temp;
+}
+const selectionSort = (arr) => {
+    for (var i = 0; i < arr.length; i++) {
+        // create a var to store the smallest value
+        let minIndex = i;
+        for (var j = i + 1; j < arr.length; j++) {
+            // compare th  is item to the next uitem in array until you find a smaller number
+            // if you find a small number while iterating, this is the "new" minimum 
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        }
+        // swap if new minimum is found at a diff index
+        if (i !== minIndex) {
+            swap(arr, i, minIndex)
+        }
+    }
+
+    return arr;
+}
+
+// [5,2,9,0,1,4,6]
+//  m, nm
+// [2, 5, 9, 0, 1, 4, 6]
