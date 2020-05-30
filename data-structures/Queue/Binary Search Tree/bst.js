c1fb5275ea46493a434ee5622b17f8b951ead931
Mike Chi <mikechi@macbook-pro-4.lan>
2020-05-30 15:54:25 -0700

    binary search trees

---
+++ data-structures/Queue/Binary Search Tree/bst.js
@@ -0,0 +1,60 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    // O(log n)
+    insert(value) {
+        let newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left
+                    }
+                } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    // O(log n)
+    find(val) {
+        if (!this.root) return false;
+        if (this.root.value === val) return this.root;
+
+        let current = this.root,
+            round = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+
+        return current;
+    }
+}
